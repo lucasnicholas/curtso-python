0cda88e74224c3efa14d2af5fd79f138a4fb2bbf
lucasnicholas <43679200+lucasnicholas@users.noreply.github.com>
2018-09-28 15:13:19 -0300

    Add files via upload

---
+++ AULA1.py
@@ -0,0 +1,83 @@
+print("EXERCICIO 1")
+kmil=10/1.61 #km em milhas
+tempo=(43/60)+30/3600 #tempo em segundos
+print("a velocidade media e de",kmil/tempo,"mill/h")
+#resposta 8.567144998929106 
+
+
+print("EXERCICIO 2")
+
+
+D=3*343 #distancia é velocidade vezes tempo
+print(" a distancia e de",D,"metros")
+#resposta 1029
+
+
+
+print("EXERCICIO 3")
+
+
+
+Delta=16+4*3*(10)
+Delta
+136
+Delta=16+4*3*(10)
+Deltar=(Delta)**(1/2)
+Deltar #raiz de delta
+# resposta 11.661903789690601
+
+y=(4-Deltar)/6
+
+print("as duas possiveos solucoes sao:")
+print("a primeira raiz e",y) 
+#raiz 1 -1.2769839649484336 
+y2=(4+Deltar)/6
+print("a segunda raiz e ",y2)
+#raiz 2 2.610317298281767
+
+
+
+print("EXERCICO 4")
+
+
+
+altura=5
+sombra=0.5
+
+angulo=sombra/altura
+print("o angulo zenital e de",angulo)
+#resposta 0.1
+
+
+print("EXERCICIO 5")
+
+
+imc=11/((0.7)**2)
+print("o imc e de",imc)
+#resposta 22.44897959183674
+
+
+
+print("EXERCICIO 6")
+
+
+# mgh=mv²/2, logo v=(2gh)**(1/2)
+
+
+velocidade=(2*9.8*3)**(1/2)
+print(" a velocidade e de ", velocidade, "m/s")
+#resposta 7.6681158050723255
+
+
+ #tempo de queda usamos v=v0+at
+tempo=velocidade/9.8
+print("o tempo e de", tempo, "segundos")
+#resposta 0.7824607964359516
+
+
+
+
+
+
+
+
